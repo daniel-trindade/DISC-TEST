7609675b8bb445bd30a4cfd655f914e486d17ae1
Daniel Trindade <danielbrunotrindade@gmail.com>
2024-09-01 20:09:04 -0300

    homepage finished

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, ButtonComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
