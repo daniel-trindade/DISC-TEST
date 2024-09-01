77456629d22836320ef67e0f9d69c9e873988305
Daniel Trindade <danielbrunotrindade@gmail.com>
2024-09-01 00:39:52 -0300

    Just Created

---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+
+}
