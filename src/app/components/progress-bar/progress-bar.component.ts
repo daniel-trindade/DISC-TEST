adaf92cc3e4c7a60c6cc0a0910765e5ddd52c337
Daniel Trindade <danielbrunotrindade@gmail.com>
2024-09-02 13:48:08 -0300

    Progress bar working

---
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-progress-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './progress-bar.component.html',
+  styleUrl: './progress-bar.component.scss'
+})
+export class ProgressBarComponent {
+  @Input() progress: number = 20;
+}
--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../components/button/button.component";
+import { ProgressBarComponent } from "../../components/progress-bar/progress-bar.component";
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, ProgressBarComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
 export class TestComponent {
-
+  option1 = "Preguiçoso"
+  option2 = "Ágil"
+  option3 = "Gentil"
+  option4 = "Feliz"
+  progressLabel: number = 15
+  progressBar: number = ((this.progressLabel*100)/30)
 }
