197f52bc6b05bc876f8f7d73f4cf274435cf38f6
Daniel Trindade <danielbrunotrindade@gmail.com>
2024-09-01 21:32:27 -0300

    Button por props working

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-
+  @Input() height: string = '40px'; // valor padrão
+  @Input() width: string = '100px'; // valor padrão
+  @Input() label: string = 'Button'; // label para o botão
 }
