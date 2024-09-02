2ed3481f2a0d70010fce1495383d71599c211e5f
Daniel Trindade <danielbrunotrindade@gmail.com>
2024-09-01 22:21:48 -0300

    main test created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { StartingTestComponent } from "./pages/starting-test/starting-test.component";
+import { TestComponent } from "./pages/test/test.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent, NavBarComponent, FooterComponent, StartingTestComponent],
+  imports: [RouterOutlet, HomePageComponent, NavBarComponent, FooterComponent, StartingTestComponent, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
