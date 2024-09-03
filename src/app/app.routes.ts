99df1e9f7bbb0db452d7a4dd20023297c9ca72cd
Daniel Trindade <danielbrunotrindade@gmail.com>
2024-09-03 17:00:13 -0300

    router working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { StartingTestComponent } from './pages/starting-test/starting-test.component';
+import { TestComponent } from './pages/test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'inicio', component: StartingTestComponent},
+  {path: 'teste', component: TestComponent}
+];
