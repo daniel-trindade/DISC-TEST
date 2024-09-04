5d8e13f430bfbc239dd5a56369c29c3e358eed99
Daniel Trindade <danielbrunotrindade@gmail.com>
2024-09-04 01:39:23 -0300

    db.json working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { StartingTestComponent } from "./pages/starting-test/starting-test.component";
 import { TestComponent } from "./pages/test/test.component";
 
+
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent, NavBarComponent, FooterComponent, StartingTestComponent, TestComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    RouterOutlet,
+    HomePageComponent,
+    NavBarComponent,
+    FooterComponent,
+    StartingTestComponent,
+    TestComponent,
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'test_disc';
+  title = "test_disc";
 }
--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { ButtonComponent } from "../../components/button/button.component";
 import { ProgressBarComponent } from "../../components/progress-bar/progress-bar.component";
 
+import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
+import { DataService } from "../../data.service";
+
 @Component({
-  selector: 'app-test',
+  selector: "app-test",
   standalone: true,
-  imports: [ButtonComponent, ProgressBarComponent],
-  templateUrl: './test.component.html',
-  styleUrl: './test.component.scss'
+  imports: [
+    ButtonComponent,
+    ProgressBarComponent,
+    HttpClientModule,
+    CommonModule,
+  ],
+  templateUrl: "./test.component.html",
+  styleUrl: "./test.component.scss",
+  providers: [DataService]
 })
 export class TestComponent {
-  option1 = "Preguiçoso"
-  option2 = "Ágil"
-  option3 = "Gentil"
-  option4 = "Feliz"
-  progressLabel: number = 15
-  progressBar: number = ((this.progressLabel*100)/30)
+  option1 = "Preguiçoso";
+  option2 = "Ágil";
+  option3 = "Gentil";
+  option4 = "Feliz";
+  progressLabel: number = 15;
+  progressBar: number = (this.progressLabel * 100) / 30;
+
+  data: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe(data => {
+      this.data = data;
+      console.log(data.questions.question_1.atributo1)
+    });
+  }
 }
